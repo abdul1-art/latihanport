64eab8c5673f552932d175b3d99199fc510d09ca
abdul1-art <abdul1125@student.abudzar.sch.id>
2026-04-09 13:14:34 +0700

    latihanport

---
+++ javascrip.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById("menu");
+let humber = document.getElementById("humber");
+let navbar = document.getElementById("navbar");
+humber.addEventListener('click', () => {
+  menu.classList.toggle('success')
+});
+
